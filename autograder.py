8bda8e61c10cc7f46096d8b1135eb50695a73552
Abhijat Bharadwaj <96429535+Keymii@users.noreply.github.com>
2023-11-16 12:53:29 +0530

    Checking validity of input and output files, colourized and formatted the output

--- autograder.py
+++ autograder.py
@@ -1,7 +1,8 @@
 import subprocess
 from pathlib import Path
-from diff import find_and_print_differences
+from diff import find_and_print_differences, colorize_line
 def run_test(input_str, expected_output, question_path):
+    
     # Compile the C++ code
     compile_command = "s++ " + str(question_path) #LQ2_D_Q1.cpp"
     # print(compile_command)
@@ -24,7 +25,7 @@ def run_test(input_str, expected_output, question_path):
 
         return passFlag
     except subprocess.TimeoutExpired:
-        print(f"Test case timed out after {timeout_sec} seconds.")
+        print(colorize_line(f"Test case timed out after {timeout_sec} seconds.",43))
         return False
 
 def read_test_cases(file_path):
@@ -34,13 +35,21 @@ def read_test_cases(file_path):
     
     test_cases = []
 
-
+    file_category = None
     for line in lines:
         if line.startswith("#testcase_input"):
+            if file_category == "output":
+                raise Exception("Input values found in output test case file, please check the test files")
             test_cases.append("")
+            if file_category == None:
+                file_category = "input"
             
         elif line.startswith("#testcase_output"):
+            if file_category == "input":
+                raise Exception("Output values found in input test case file, please check the test files")
             test_cases.append("")
+            if file_category == None:
+                file_category = "output"
         else:
             test_cases[-1] += line
     
@@ -52,38 +61,41 @@ def autograde(path):
     enumerated_test_cases = enumerate(zip(input_test_cases, output_test_cases), 1)
     for i, (input_str, expected_output) in enumerated_test_cases:
         try:
+            print(colorize_line(f"\nEvaluating test case {i}\n",93))
             testSuccess = run_test(input_str, expected_output, path) 
         except Exception as e:
             testSuccess = False
             print("Error occured")
             print(e)
         if testSuccess:
-            print(f"Test case {i}: Passed")
+            print(f"Test case {i}: Passed\n")
         else:
-            print(f"Test case {i}: Failed")
+            print(f"Test case {i}: Failed\n")
 
 def search_cpp_files(root_folder, filename_to_match):
     root = Path(root_folder)
-    cpp_file_path_list=[]
+    cpp_file_path_dict={}
     for roll_number_path in root.iterdir():
         if roll_number_path.is_dir():
             found = False
             for file_path in roll_number_path.rglob('*.cpp'):
                 if filename_to_match in file_path.name:
-                    print("-"*100)
-                    print(f"{roll_number_path.name}: present")
-                    cpp_file_path_list.append(file_path)
-                    #autograde(file_path)
+                    cpp_file_path_dict[roll_number_path.name]=file_path
                     found = True
                     break
             if not found:
-                print("-"*100)
-                print(f"{roll_number_path.name}: absent")
-    return cpp_file_path_list
+                cpp_file_path_dict[roll_number_path.name]="absent"
+                
+    return cpp_file_path_dict
 
 if __name__ == "__main__":
-    file_paths = search_cpp_files('../Day4_Thurs_9_Nov-20231110T172808Z-001', 'Q2')
-    for path in file_paths:
-        autograde(path)
+    file_paths = search_cpp_files('../Day4_Thurs_9_Nov-20231110T172808Z-001', 'Q3b')
+    for roll in file_paths:
+        print("-"*100)
+        path = file_paths[roll]
+        message = f"\033[1m{roll}\033[22m: {path if path == 'absent' else 'present'}"
+        print(message)
+        if path !="absent":
+            autograde(path)
 
 
--- diff.py
+++ diff.py
@@ -9,25 +9,25 @@ def find_and_print_differences(actual_output, expected_output):
 
     max_len = max(len(lines_actual), len(lines_expected))
     
-    print("Line   | Actual Output   | Expected Output")
-    print("-------|-----------------|----------------")
+    print("Line   | Actual Output        | Expected Output")
+    print("-------|----------------------|---------------------")
 
     for i, (line_actual, line_expected) in enumerate(zip(lines_actual + [''] * (max_len - len(lines_actual)), lines_expected + [''] * (max_len - len(lines_expected))), 1):
         if line_actual != line_expected:
             line_actual_colored = colorize_line(line_actual, '91')  # ANSI code for red text
             line_expected_colored = colorize_line(line_expected, '92')  # ANSI code for green text
-            print(f"{i:<7}| {line_actual_colored.ljust(16)} | {line_expected_colored}")
+            print(f"{i:<7}| {line_actual_colored.ljust(29)} | {line_expected_colored}")
         else:
-            print(f"{i:<7}| {line_actual.ljust(16)} | {line_expected}")
+            print(f"{i:<7}| {line_actual.ljust(20)} | {line_expected}")
 
 if __name__=="__main__":
 # Example usage:
     actual_output = """Line 1
-    Line 2
-    Line 3"""
+Line 2
+Line 3"""
 
     expected_output = """Line 1
-    Modified Line 2
-    Line 3"""
+Modified Line 2
+Line 3"""
 
     find_and_print_differences(actual_output, expected_output)
