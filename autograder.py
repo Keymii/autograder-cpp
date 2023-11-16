68e3db0072e266e4f2593a08397b08d5c4aa5f44
Abhijat Bharadwaj <96429535+Keymii@users.noreply.github.com>
2023-11-16 13:19:02 +0530

    declared color configurations in ansi_colors.py

--- autograder.py
+++ autograder.py
@@ -1,17 +1,20 @@
 import subprocess
 from pathlib import Path
 from diff import find_and_print_differences, colorize_line
+from ansi_colors import ERROR_COLOR, TEST_EVAL_COLOR
 def run_test(input_str, expected_output, question_path):
     
     # Compile the C++ code
-    compile_command = "s++ " + str(question_path) #LQ2_D_Q1.cpp"
+    compile_command = "s++ " + str(question_path)
     # print(compile_command)
     subprocess.run(compile_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     
     # Run the compiled program with input
     run_command = f"./a.out"
 
+    # If no output found, wait for 5 seconds before throwing a timeout and moving on. This ensures program moves on even if DUT is stuck in infinite loop
     timeout_sec=5
+    
 
     try:
         process = subprocess.run(run_command, input=input_str, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, timeout = timeout_sec)
@@ -25,7 +28,7 @@ def run_test(input_str, expected_output, question_path):
 
         return passFlag
     except subprocess.TimeoutExpired:
-        print(colorize_line(f"Test case timed out after {timeout_sec} seconds.",43))
+        print(colorize_line(f"Test case timed out after {timeout_sec} seconds.",ERROR_COLOR))
         return False
 
 def read_test_cases(file_path):
@@ -39,14 +42,14 @@ def read_test_cases(file_path):
     for line in lines:
         if line.startswith("#testcase_input"):
             if file_category == "output":
-                raise Exception("Input values found in output test case file, please check the test files")
+                raise TestBenchValidationException("Input values found in output test case file, please check the test files")
             test_cases.append("")
             if file_category == None:
                 file_category = "input"
             
         elif line.startswith("#testcase_output"):
             if file_category == "input":
-                raise Exception("Output values found in input test case file, please check the test files")
+                raise TestBenchValidationException("Output values found in input test case file, please check the test files")
             test_cases.append("")
             if file_category == None:
                 file_category = "output"
@@ -61,11 +64,11 @@ def autograde(path):
     enumerated_test_cases = enumerate(zip(input_test_cases, output_test_cases), 1)
     for i, (input_str, expected_output) in enumerated_test_cases:
         try:
-            print(colorize_line(f"\nEvaluating test case {i}\n",93))
+            print(colorize_line(f"\nEvaluating test case {i}\n",TEST_EVAL_COLOR))
             testSuccess = run_test(input_str, expected_output, path) 
         except Exception as e:
             testSuccess = False
-            print("Error occured")
+            print(colorize_line("An error occured while evaluting the test case", ERROR_COLOR))
             print(e)
         if testSuccess:
             print(f"Test case {i}: Passed\n")
@@ -85,9 +88,14 @@ def search_cpp_files(root_folder, filename_to_match):
                     break
             if not found:
                 cpp_file_path_dict[roll_number_path.name]="absent"
-                
     return cpp_file_path_dict
 
+class TestBenchValidationException(Exception):
+    def __init__(self, message):
+        self.message = message
+    def __str__(self) -> str:
+        return colorize_line(self.message, ERROR_COLOR)
+        
 if __name__ == "__main__":
     file_paths = search_cpp_files('../Day4_Thurs_9_Nov-20231110T172808Z-001', 'Q3b')
     for roll in file_paths:
--- diff.py
+++ diff.py
@@ -1,5 +1,5 @@
 from difflib import unified_diff
-
+from ansi_colors import TEXT_FOUND_COLOR, TEXT_EXPECTED_COLOR
 def colorize_line(line, color_code):
     return f"\033[{color_code}m{line}\033[0m"
 
@@ -14,8 +14,8 @@ def find_and_print_differences(actual_output, expected_output):
 
     for i, (line_actual, line_expected) in enumerate(zip(lines_actual + [''] * (max_len - len(lines_actual)), lines_expected + [''] * (max_len - len(lines_expected))), 1):
         if line_actual != line_expected:
-            line_actual_colored = colorize_line(line_actual, '91')  # ANSI code for red text
-            line_expected_colored = colorize_line(line_expected, '92')  # ANSI code for green text
+            line_actual_colored = colorize_line(line_actual, TEXT_FOUND_COLOR) 
+            line_expected_colored = colorize_line(line_expected, TEXT_EXPECTED_COLOR) 
             print(f"{i:<7}| {line_actual_colored.ljust(29)} | {line_expected_colored}")
         else:
             print(f"{i:<7}| {line_actual.ljust(20)} | {line_expected}")
