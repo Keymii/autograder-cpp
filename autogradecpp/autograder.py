a8a6e053cbd96ffc1825941335245bb1b468dd07
Abhijat Bharadwaj <96429535+Keymii@users.noreply.github.com>
2023-11-16 16:36:02 +0530

    shifted code files to autogradecpp/ directory - main.py serves as user interface - autogradecpp serves as a library

--- autogradecpp/autograder.py
+++ autogradecpp/autograder.py
@@ -49,6 +49,19 @@ def autograde(path, input_test_cases, output_test_cases, run_mode="standard", ti
                 print()
     print(ansi_colors.colorize_line(f"Passed {num_test_passed}/{total_cases}",ansi_colors.BACKGROUND_COLORS['blue']))
         
+def grade_root_dir(ROOT_FOLDER_PATH, FILENAME_TO_MATCH, TEST_INPUT_FILE_PATH, TEST_OUTPUT_FILE_PATH, RUN_MODE, WAIT_TIMEOUT_SEC):
+    file_paths = search_cpp_files(ROOT_FOLDER_PATH, FILENAME_TO_MATCH)
+    input_test_cases = read_test_cases(TEST_INPUT_FILE_PATH)
+    output_test_cases = read_test_cases(TEST_OUTPUT_FILE_PATH)
+    for roll in file_paths:
+        print('\n'+"-"*100)
+        path = file_paths[roll]
+        message =ansi_colors.colorize_line( ansi_colors.colorize_line(f"{roll}: {path if path == 'absent' else 'present'}", ansi_colors.FOREGROUND_COLORS['yellow']), ansi_colors.FOREGROUND_COLORS['bold'])
+        print(message)
+        if path !="absent":
+            autograde(path, input_test_cases, output_test_cases, run_mode=RUN_MODE, timeout_sec=WAIT_TIMEOUT_SEC)
+  
+
 if __name__ == "__main__":
     file_paths = search_cpp_files('../Day4_Thurs_9_Nov-20231110T172808Z-001', 'Q1')
     input_test_cases = read_test_cases("./inp.txt")
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import sys
 sys.path.insert(1, './autogradecpp')# caution: path[0] is reserved for script path (or '' in REPL)
-import autogradecpp.autograder as autograder
-import autogradecpp.file_handler as file_handler
-import autogradecpp.ansi_colors as ansi_colors
+from autogradecpp import autograder, file_handler, ansi_colors
 
 
 ROOT_FOLDER_PATH = '../Day4_Thurs_9_Nov-20231110T172808Z-001'
@@ -14,18 +12,6 @@ TEST_OUTPUT_FILE_PATH = "./output.txt"
 RUN_MODE = "standard" #Can choose from 'summary', 'concise', 'standard', 'verbose'
 
 WAIT_TIMEOUT_SEC = 5 #If a file is taking too long to compile, fail it after this many seconds.
-
-def grade_root(ROOT_FOLDER_PATH, FILENAME_TO_MATCH, TEST_INPUT_FILE_PATH, TEST_OUTPUT_FILE_PATH, RUN_MODE, WAIT_TIMEOUT_SEC):
-    file_paths = file_handler.search_cpp_files(ROOT_FOLDER_PATH, FILENAME_TO_MATCH)
-    input_test_cases = file_handler.read_test_cases(TEST_INPUT_FILE_PATH)
-    output_test_cases = file_handler.read_test_cases(TEST_OUTPUT_FILE_PATH)
-    for roll in file_paths:
-        print('\n'+"-"*100)
-        path = file_paths[roll]
-        message =ansi_colors.colorize_line( ansi_colors.colorize_line(f"{roll}: {path if path == 'absent' else 'present'}", ansi_colors.FOREGROUND_COLORS['yellow']), ansi_colors.FOREGROUND_COLORS['bold'])
-        print(message)
-        if path !="absent":
-            autograder.autograde(path, input_test_cases, output_test_cases, run_mode=RUN_MODE, timeout_sec=WAIT_TIMEOUT_SEC)
-            
+          
 if __name__=="__main__":
-    grade_root(ROOT_FOLDER_PATH, FILENAME_TO_MATCH, TEST_INPUT_FILE_PATH, TEST_OUTPUT_FILE_PATH, RUN_MODE, WAIT_TIMEOUT_SEC)
+    autograder.grade_root_dir(ROOT_FOLDER_PATH, FILENAME_TO_MATCH, TEST_INPUT_FILE_PATH, TEST_OUTPUT_FILE_PATH, RUN_MODE, WAIT_TIMEOUT_SEC)
