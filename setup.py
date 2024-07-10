0222e45fd2817ba50a9b8aaf29d34dc32d5533c1
Abhijat Bharadwaj <96429535+Keymii@users.noreply.github.com>
2024-07-10 23:52:03 +0530

    add long desc.

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import find_packages, setup
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='autogradercpp',
     packages=find_packages(
         include=['autogradercpp']
     ),
-    version='0.1.0',
+    version='0.1.1',
     description='An autograder library for evaluating C++ programs.',
     author='Abhijat Bharadwaj (Keymii)',
     author_email='bharadwaj.abhijat@yahoo.com',
@@ -14,5 +19,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    install_requires=[]
+    install_requires=[],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
